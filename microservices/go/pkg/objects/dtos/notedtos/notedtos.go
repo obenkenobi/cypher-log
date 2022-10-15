d015464a2c861cf20559da0d00a8f6cd9c909fac
oren <orenbm11@gmail.com>
2022-10-15 00:22:02 -0400

    Wire note repository

---
+++ microservices/go/pkg/objects/dtos/notedtos/notedtos.go
@@ -0,0 +1,32 @@
+package notedtos
+
+import "github.com/obenkenobi/cypher-log/microservices/go/pkg/objects/embedded"
+
+type CoreNoteDto struct {
+	Title string `json:"title" binding:"required,alphanumunicode,min=4,max=1000"`
+}
+
+type CoreNoteDetailsDto struct {
+	CoreNoteDto
+	Text string `json:"text" binding:"max=40000"`
+}
+
+type NoteCreateDto struct {
+	CoreNoteDetailsDto
+}
+
+type NoteUpdateDto struct {
+	embedded.BaseId
+	CoreNoteDetailsDto
+}
+
+type NoteReadDto struct {
+	embedded.BaseCRUDObject
+	CoreNoteDto
+	TextPreview string
+}
+
+type NoteDetailsDto struct {
+	embedded.BaseCRUDObject
+	CoreNoteDetailsDto
+}
