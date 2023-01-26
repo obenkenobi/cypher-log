e0ef9b23796f5595827b91686e30298b59f098da
oren <orenbm11@gmail.com>
2023-01-25 23:58:37 -0500

    Add function caller to logs

--- microservices/go/pkg/logger/logger.go
+++ microservices/go/pkg/logger/logger.go
@@ -6,12 +6,20 @@ import (
 )
 
 func getTextFormatter() *log.TextFormatter {
-	return &log.TextFormatter{DisableColors: false, ForceColors: true, FullTimestamp: true}
+	return &log.TextFormatter{
+		DisableColors:    false,
+		ForceColors:      true,
+		FullTimestamp:    true,
+		PadLevelText:     true,
+		QuoteEmptyFields: true,
+		ForceQuote:       true,
+	}
 }
 
-func getJsonFormatter() *log.JSONFormatter { return &log.JSONFormatter{} }
+func getJsonFormatter() *log.JSONFormatter { return &log.JSONFormatter{PrettyPrint: true} }
 
 func configureLoggerFromEnv(logger *log.Logger) {
+	logger.SetReportCaller(true)
 	if environment.IsProduction() {
 		logger.SetFormatter(getJsonFormatter())
 		logger.SetLevel(log.InfoLevel)
@@ -29,8 +37,9 @@ func configureLoggerFromEnv(logger *log.Logger) {
 
 func newLogger() *log.Logger {
 	logger := log.New()
+	logger.SetReportCaller(true)
 	logger.SetFormatter(getTextFormatter())
-	logger.SetLevel(log.DebugLevel)
+	logger.SetLevel(log.InfoLevel)
 	return logger
 }
 
