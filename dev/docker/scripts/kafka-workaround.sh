09ac93fbf02ae9dbb9956711fbd46931766db99c
oren <orenbm11@gmail.com>
2023-01-27 22:43:43 -0500

    delete kafka workaround

--- dev/docker/scripts/kafka-workaround.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-# Disable zookeeper connection
-sed -i '/KAFKA_ZOOKEEPER_CONNECT/d' /etc/confluent/docker/configure
-sed -i 's/cub zk-ready/echo ignore zk-ready/' /etc/confluent/docker/ensure
-
-# Required for KRaft
-echo "kafka-storage format --ignore-formatted -t NqnEdODVKkiLTfJvqd1uqQ== -c /etc/kafka/kafka.properties" >> /etc/confluent/docker/ensure
