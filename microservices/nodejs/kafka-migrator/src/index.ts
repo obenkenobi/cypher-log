b9bdd2f4ff57df68b276a69c8682f2bb3b6af7a2
oren <orenbm11@gmail.com>
2023-01-27 21:23:36 -0500

    basic kafka admin connect

--- microservices/nodejs/kafka-migrator/src/index.ts
+++ microservices/nodejs/kafka-migrator/src/index.ts
@@ -1,1 +1,31 @@
-console.log("Hello world!!!")
+import { Kafka, SASLOptions } from 'kafkajs'
+import * as dotenv from 'dotenv'
+
+
+if (process.env.IGNORE_ENV_FILE !== "true") {
+  dotenv.config()
+}
+
+const { KAFKA_USERNAME: username, KAFKA_PASSWORD: password } = process.env;
+const sasl: SASLOptions | undefined = username && password ? { username, password, mechanism: 'plain' } : undefined;
+const ssl = !!sasl;
+
+console.log((process.env.KAFKA_BOOTSTRAP_SERVERS || "").split(","))
+const kafka = new Kafka({
+  clientId: 'npm-slack-notifier',
+  brokers: (process.env.KAFKA_BOOTSTRAP_SERVERS || "").split(","),
+  ssl,
+  sasl
+});
+
+const admin = kafka.admin();
+
+(async ()=> {
+  await admin.connect()
+  try {
+    console.log("Hello world!!!")
+  } finally {
+    await admin.disconnect()
+  }
+})()
+
