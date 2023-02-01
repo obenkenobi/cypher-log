bcc5d354fbd98fb9be8380e9f6cbea86c971f4e7
oren <orenbm11@gmail.com>
2023-01-31 23:57:37 -0500

    add todo for note dtos

---
+++ microservices/go/cmd/uiservice/ClientApp/src/models/dtos/note.ts
@@ -0,0 +1,52 @@
+/*
+Todo: create interfaces based on the following Go structs
+ package notedtos
+
+import "github.com/obenkenobi/cypher-log/microservices/go/pkg/objects/embedded"
+
+type CoreNoteDto struct {
+	Title string `json:"title" binding:"required,min=4,max=1000"`
+}
+
+func NewCoreNoteDto(title string) CoreNoteDto {
+	return CoreNoteDto{Title: title}
+}
+
+type CoreNoteDetailsDto struct {
+	CoreNoteDto
+	Text string `json:"text" binding:"max=40000"`
+}
+
+func NewCoreNoteDetailsDto(title string, text string) CoreNoteDetailsDto {
+	return CoreNoteDetailsDto{
+		CoreNoteDto: NewCoreNoteDto(title),
+		Text:        text,
+	}
+}
+
+type NoteCreateDto struct {
+	CoreNoteDetailsDto
+}
+
+type NoteUpdateDto struct {
+	embedded.BaseId
+	CoreNoteDetailsDto
+}
+
+type NotePreviewDto struct {
+	embedded.BaseCRUDObject
+	CoreNoteDto
+	TextPreview string
+}
+
+type NoteReadDto struct {
+	embedded.BaseCRUDObject
+	CoreNoteDetailsDto
+}
+
+type NoteIdDto struct {
+	embedded.BaseRequiredId
+}
+
+ */
+
