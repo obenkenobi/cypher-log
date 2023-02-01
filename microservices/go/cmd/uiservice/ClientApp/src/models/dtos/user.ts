3cfb0202fce45e26023c32d88c7652adfd16ae49
oren <orenbm11@gmail.com>
2023-01-31 23:44:12 -0500

    add user dtos

--- microservices/go/cmd/uiservice/ClientApp/src/models/dtos/user.ts
+++ microservices/go/cmd/uiservice/ClientApp/src/models/dtos/user.ts
@@ -1,1 +1,9 @@
+interface UserIdentityDto extends UserReadDto, BaseUserAuthId {
+  authorities: string[]
+}
+
+interface UserReadDto extends BaseUserPublicDto {
+  exists: boolean
+}
+
 interface UserSaveDto extends BaseUserCommon {}
