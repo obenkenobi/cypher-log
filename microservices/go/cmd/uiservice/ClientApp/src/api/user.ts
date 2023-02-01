0e6a611b255138bd05f4bb0dacca8c8990547a3f
oren <orenbm11@gmail.com>
2023-02-01 01:11:18 -0500

    add user api calls

--- microservices/go/cmd/uiservice/ClientApp/src/api/user.ts
+++ microservices/go/cmd/uiservice/ClientApp/src/api/user.ts
@@ -1,1 +1,20 @@
-//Todo: implement user api calls
+import axios from '../axios'
+//Todo: handle errors
+
+const prefix = "/api/userservice"
+
+export function addUser(req: UserSaveDto): Promise<UserReadDto> {
+  return axios.post<UserSaveDto, UserReadDto>(`${prefix}/v1/user`, req)
+}
+
+export function updateUser(req: UserSaveDto): Promise<UserReadDto> {
+  return axios.put<UserSaveDto, UserReadDto>(`${prefix}/v1/user`, req)
+}
+
+export function deleteUser(): Promise<UserReadDto> {
+  return axios.delete<any, UserReadDto>(`${prefix}/v1/user`)
+}
+
+export function getIdentity(): Promise<UserIdentityDto> {
+  return axios.get<any, UserIdentityDto>(`${prefix}/v1/user`)
+}
